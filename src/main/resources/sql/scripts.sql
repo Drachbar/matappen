e3e23de80920e4a733702c217297bd5e8abe3b2b
Mattias <drachbar@gmail.com>
2024-05-04 23:15:33 +0200

    Uppdatera sql script-filen

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -8,9 +8,11 @@ CREATE TABLE `user`
 (
     `user_id`   int          NOT NULL AUTO_INCREMENT,
     `name`      varchar(100) NOT NULL,
-    `email`     varchar(100) NOT NULL,
+    `email`     varchar(254) NOT NULL,
     `password`  varchar(500) NOT NULL,
-    `role`      varchar(100) NOT NULL,
-    `create_dt` date DEFAULT NULL,
-    PRIMARY KEY (`user_id`)
-);
+    `role`      varchar(100) NOT NULL DEFAULT 'user',
+    `create_dt` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `activated` tinyint               DEFAULT '0',
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `email` (`email`)
+)
