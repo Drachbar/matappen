df3ce1a7c022d949c15f1bdba0270ebdafdec013
Mattias <drachbar@gmail.com>
2024-05-05 23:28:10 +0200

    Returnera användarnamnet för användaren vid testa autentisering

--- src/main/java/se/matappen/matappen/controller/TestController.java
+++ src/main/java/se/matappen/matappen/controller/TestController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -49,6 +51,7 @@ public class TestController {
 
     @GetMapping("/testAuthentication")
     public ResponseEntity<String> testAuthentication() {
-        return ResponseEntity.ok("Autentiserad");
+        Authentication autentication = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok(autentication.getName());
     }
 }
