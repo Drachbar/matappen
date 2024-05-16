55de23cbbc590d436c4595a3fe7f2aa8657cd7a9
Mattias <drachbar@gmail.com>
2024-05-16 23:19:24 +0200

    Returnera json/text för autentisering istället för html

--- src/main/java/se/matappen/matappen/config/ProjectSecurityConfig.java
+++ src/main/java/se/matappen/matappen/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package se.matappen.matappen.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -8,11 +9,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
+import se.matappen.matappen.security.SimpleAuthenticationEntryPoint;
+import se.matappen.matappen.security.SimpleAuthenticationFailureHandler;
+import se.matappen.matappen.security.SimpleAuthenticationSuccessHandler;
 
 import java.util.Collections;
 
 @Component
+@AllArgsConstructor
 public class ProjectSecurityConfig {
+
+    private SimpleAuthenticationSuccessHandler successHandler;
+    private SimpleAuthenticationFailureHandler failureHandler;
+    private SimpleAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(final HttpSecurity http) throws Exception {
@@ -29,7 +38,15 @@ public class ProjectSecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/v1/login", "api/v1/testAuthentication", "api/v1/recipe/add").authenticated()
                         .anyRequest().permitAll())
-                .formLogin(form -> form.loginProcessingUrl("/api/v1/login"));
+                .formLogin(form -> form
+                        .loginProcessingUrl("/api/v1/login")
+                        .successHandler(successHandler)
+                        .failureHandler(failureHandler)
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(authenticationEntryPoint)
+                );
 
         return http.build();
     }
