2dbf9300f0bee0d309fce8640644965d998eb5e5
Mattias <drachbar@gmail.com>
2024-12-02 22:28:34 +0100

    Skicka med lista på bildnamn

--- src/main/java/se/matappen/matappen/dto/RecipeDetailedDto.java
+++ src/main/java/se/matappen/matappen/dto/RecipeDetailedDto.java
@@ -3,5 +3,5 @@ package se.matappen.matappen.dto;
 import java.util.List;
 
 public record RecipeDetailedDto(RecipeDto recipeDto, List<RecipeStepDto> recipeStepsDto,
-                                List<IngredientSectionDto> ingredientSectionsDto) {
+                                List<IngredientSectionDto> ingredientSectionsDto, List<ImagesDto> imagesDto) {
 }
