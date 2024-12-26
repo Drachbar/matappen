c4752d41d46fb3004467ae96a0b1efe8a85e03b5
Mattias <drachbar@gmail.com>
2024-12-26 11:24:36 +0100

    Sortera receptet

--- frontend/src/app/components/recipe/recipe.component.ts
+++ frontend/src/app/components/recipe/recipe.component.ts
@@ -2,30 +2,40 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {NgIf} from "@angular/common";
+import {Recipe} from "../../model/recipe";
 
 @Component({
-    selector: 'app-recipe',
+  selector: 'app-recipe',
   imports: [
     NgIf
   ],
-    templateUrl: './recipe.component.html',
-    styleUrl: './recipe.component.scss'
+  templateUrl: './recipe.component.html',
+  styleUrl: './recipe.component.scss'
 })
 export class RecipeComponent implements OnInit {
-  recipe: any = null;
+  recipe?: Recipe;
+
   constructor(private route: ActivatedRoute, private http: HttpClient) {
   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')!;
-    this.http.get(`api/v1/recipe/getRecipeById?id=${id}`).subscribe({
-      next: (response) => {
-        this.recipe = response;
-        console.log('Data skickades!', response);
+    this.http.get<Recipe>(`api/v1/recipe/getRecipeById?id=${id}`).subscribe({
+      next: (data: Recipe) => {
+        this.recipe = this.sortRecipe(data)
       },
       error: (error) => console.error('Det blev ett fel!', error)
     })
   }
 
+  sortRecipe(recipe: Recipe): Recipe {
+    recipe.recipeStepsDto.sort((a, b) => a.stepOrder - b.stepOrder);
+    recipe.ingredientSectionsDto.forEach(section =>
+      section.ingredients.sort((a, b) => a.ingredientOrder - b.ingredientOrder)
+    );
+    recipe.ingredientSectionsDto.sort((a, b) => a.sectionOrder - b.sectionOrder);
+    recipe.imagesDto.sort((a, b) => a.order - b.order);
+    return recipe;
+  }
 
 }
--- frontend/src/app/model/recipe.ts
+++ frontend/src/app/model/recipe.ts
@@ -5,3 +5,47 @@ export type recipeSummary = {
   nameRecipe: string;
   imageUrl: string | null;
 }[]
+
+export type Recipe = {
+  recipeDto: RecipeDto;
+  recipeStepsDto: RecipeStep[];
+  ingredientSectionsDto: IngredientSection[];
+  imagesDto: Image[];
+};
+
+export type RecipeDto = {
+  id: number;
+  nameRecipe: string;
+  description: string;
+  creatorName: string;
+  imageUrl: string | null;
+};
+
+export type RecipeStep = {
+  description: string;
+  stepOrder: number;
+  id: number;
+};
+
+export type IngredientSection = {
+  name: string;
+  sectionOrder: number;
+  ingredients: Ingredient[];
+  id: number;
+};
+
+export type Ingredient = {
+  name: string;
+  unit: string | null;
+  ingredientOrder: number;
+  amount: number | null;
+  id: number;
+};
+
+export type Image = {
+  name: string;
+  width: number;
+  height: number;
+  order: number;
+  id: number;
+};
