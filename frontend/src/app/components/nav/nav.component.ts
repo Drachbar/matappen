c3f454219cfc54f922038548ac116835c6a6c304
Drachbar <60190352+Drachbar@users.noreply.github.com>
2025-02-27 22:41:43 +0100

    Pr 18 (#19)
    * ändra navbar
    
    * förbättra formulär på skapa recept
    
    * fixa rätt skapa recept
    
    * fixa sök
    
    * ta bort dubblett av step
    
    ---------
    
    Co-authored-by: Victoria <victoriafingal@hotmail.se>

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,32 +1,30 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink, RouterLinkActive} from "@angular/router";
-import {AuthStateService} from "../../services/auth-state.service";
-import {NgIf} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import { Component, inject } from "@angular/core";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthStateService } from "../../services/auth-state.service";
+import { FormsModule } from "@angular/forms";
+import { NgbCollapseModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-nav',
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    NgIf,
-    FormsModule
-  ],
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  selector: "app-nav",
+  imports: [RouterLink, RouterLinkActive, FormsModule, NgbCollapseModule],
+  templateUrl: "./nav.component.html",
+  styleUrl: "./nav.component.scss",
 })
 export class NavComponent {
-  authState = inject(AuthStateService);
-  searchQuery: string = '';
+  isCollapsed = true;
 
-  constructor(private router: Router) {
-  }
+  authState = inject(AuthStateService);
+  searchQuery: string = "";
+
+  constructor(private router: Router) {}
 
   performSearch() {
-    console.log(this.searchQuery)
+    console.log(this.searchQuery);
 
     if (this.searchQuery) {
-      this.router.navigate(['recipe/search-recipe'], {queryParams: {search: this.searchQuery}});
+      this.router.navigate(["recipe/search-recipe"], {
+        queryParams: { search: this.searchQuery },
+      });
     }
   }
 }
